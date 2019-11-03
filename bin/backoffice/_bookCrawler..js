158d443ac4d38d4319894948c94d26ab06d6ae4b
Claude.fauconnet <claude.fauconnet@neuf.fr>
2019-11-03 21:25:10 +0100

    backoffice

--- bin/backoffice/_bookCrawler..js
+++ bin/backoffice/_bookCrawler..js
@@ -1,125 +1,122 @@
-
-const async=require("async");
+const async = require("async");
 const util = require("./util.");
 const socket = require('../../routes/socket.js');
 const request = require('request');
+const PDFParser = require("pdf2json");
+const ndjson = require('ndjson');
 
 
 var bookCrawler = {
 
     indexSource: function (config, callback) {
+        var pdfPath=config.connector.filePath;
+        var elasticUrl = options.indexation.elasticUrl;
+        var pdfText="";
+        var docTitle="";
+        var pdfPages=
+        async.series([
+            
+                //getDocTitle
+            function (callbackSeries) {
+                var p = pdfPath.lastIndexOf("\\");
+                if (p < 0)
+                    p = pdfPath.lastIndexOf("/");//unix
+                var docTitle = pdfPath.substring(p + 1)
+                docTitle = docTitle.substring(0, docTitle.indexOf("."))
 
+                return callbackSeries()
 
-        var index = config.general.index;
-        var sqlProxy;
-
-       if (config.connector.subType="mySQL"){
-           sqlProxy=mySQLproxy;
-       }else{
-           return callback("this SQL connector is not allowed");
-       }
-
-        var defaultFetchSize = 300;
-        var fetchSize = config.connector.fetchSize || defaultFetchSize;
-        var schemaProperties = config.schema.mappings[index].properties;
-        var fields = Object.keys(schemaProperties);
-
-
-        var bulkStr = "";
-        var totalIndexedRecords = 0
-        var offset = 0;
-        var resultSize = 1;
-        var t0 = new Date().getTime();
-        async.whilst(
-            function () {//test
-                return resultSize > 0;
             },
-            function (callbackWhilst) {//iterate
-                var sqlFetch = config.connector.sql + " limit " + fetchSize + " offset " + offset;
+            
+            //parse pdf
+            function (callbackSeries) {
+                bookCrawler.parsePdf(pdfPath, function (err, result) {
+                    if (err)
+                        return callbackSeries(err);
+                    pdfText = result;
+                    return callbackSeries();
+                })
+
+            },
+            
+            //split text in pages
+            function (callbackSeries) {
+                bookCrawler.splitPdfTextInPages(pdfText, function (err, result) {
+                    if (err)
+                        return callbackSeries(err);
+                    pdfPages = result;
+                    return callbackSeries();
+                })
+
+            },
 
 
-                sqlProxy.exec(config.connector.connOptions, sqlFetch, function (err, result) {
-                    if (err) {
-                        callback(err);
-                        return;
-                    }
+            // run indexation of each page
+            function (callbackSeries) {
+                var ndjsonStr = ""
+                var serialize = ndjson.serialize();
+                serialize.on('data', function (line) {
+                    ndjsonStr += line; // line is a line of stringified JSON with a newline delimiter at the end
+                })
 
-                    resultSize = result.length;
-                    if (resultSize == 0) {
-                        return callback(null, "end");
-                    }
+                var str = ""
+                pdfPages.forEach(function (page, pageIndex) {
+                    var id = docTitle + "_" + (pageIndex + 1)
+                    var title = docTitle + "_" + (pageIndex + 1)
 
-                    offset += resultSize;
-                    totalIndexedRecords += resultSize;
+                    str += JSON.stringify({index: {"_index": index, _type: index, "_id": id}}) + "\r\n"
+                    str += JSON.stringify({title: title,  page: "page " + (pageIndex + 1), content: page}) + "\r\n"
 
-                    var bulkStr = "";
+                    //   serialize.write({"_index": index, "_id": id})
+                    //   serialize.write({title: docTitle, pdfPath: docPath, page: (pageIndex + 1), content: page})
 
-                    // contcat all fields values in content field
-                        result.forEach(function (line) {
-                            var record = {};
-                            var content = "";
+                })
+                serialize.end();
+                var options = {
+                    method: 'POST',
+                    body: str,
+                    encoding: null,
+                    headers: {
+                        'content-type': 'application/json'
+                    },
 
-                            fields.forEach(function (field) {
-                                var value = line[field];
-                                if (!value)
-                                    return;
-                                if (value == "0000-00-00")
-                                    return;
-                                content += " " + value;
-                                record[field] = value;
+                    url: elasticUrl+"_bulk"
+                };
 
-                            })
-                            record["attachement.content"] = content;
-                            var incrementRecordId = util.getStringHash(content);
-                            var id = "R" + incrementRecordId;
+                request(options, function (error, response, body) {
 
-                            bulkStr += JSON.stringify({index: {_index: index, _type: index, _id: id}}) + "\r\n"
-                            bulkStr += JSON.stringify(record) + "\r\n"
+                    if (error)
+                        return callbackSeries(error);
 
-                        })
-
-                        var options = {
-                            method: 'POST',
-                            body: bulkStr,
-                            encoding: null,
-                            headers: {
-                                'content-type': 'application/json'
-                            },
-                            url: elasticUrl + "_bulk"
-                        };
-
-                        request(options, function (error, response, body) {
-
-                            if (error)
-                                return callback(err);
-                            var message = "indexed " + totalIndexedRecords.length+" records ";
-                            socket.message(message)
-                            return callback()
-                        })
+                    return callbackSeries(null );
+                })
+            }
 
 
-                    }
-
-                ,
-
-                    function (err, result) {//end
-                        if (err) {
-                            callback(err);
-
-                        } else {
-                            var duration = new Date().getTime() - t0;
-                            var message = "*** indexation done : " + totalIndexedRecords.length  +" records  in " + duration + " msec.";
-                            callback(null, "done");
-                        }
-
-                    }
-
-                );
-            })
-    }
+        ], function (err) {
+            callback(err);
+        })
 
 
+    },
+    parsePdf: function (pdfPath, callback) {
+        let pdfParser = new PDFParser(this, 1);
 
+        pdfParser.on("pdfParser_dataError", errData => callback(errData.parserError));
+        pdfParser.on("pdfParser_dataReady", pdfData => {
+            var text = pdfParser.getRawTextContent();
+            callback(null, text)
+
+        });
+        pdfParser.loadPDF(pdfPath);
+    },
+    splitPdfTextInPages: function (pdfText, callback) {
+        var pages = [];
+        var regex = /----------------Page \([0-9]+\) Break----------------/
+        pages = pdfText.split(regex);
+        callback(null, pages)
+
+    },
 
 
 }
