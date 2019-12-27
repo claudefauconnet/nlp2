43e14a008eb13e07e07f77ab80c67db69e8e5d34
Claude.fauconnet <claude.fauconnet@neuf.fr>
2019-12-27 12:04:04 +0100

    graph stats

--- bin/backoffice/annotator_regex..js
+++ bin/backoffice/annotator_regex..js
@@ -1,3 +1,7 @@
+
+//https://www.bp.com/content/dam/bp/country-sites/en_az/azerbaijan/home/pdfs/esias/sd/sd2/units_and_abbreviations.pdf
+//https://www.spe.org/authors/docs/symbols.pdf
+
 var fs = require('fs');
 var async = require('async');
 var request = require('request');
@@ -5,27 +9,151 @@ var ndjson = require('ndjson');
 const socket = require('../../routes/socket.js');
 const elasticRestProxy = require('../elasticRestProxy..js')
 
+
 var annotator_regex = {
 
 
+    annotateCorpus: function (globalOptions, callback) {
+        var count = 0;
+        var i = 0;
+        var fetchSize = 200;
+        var from = 0;
+        var allMeasurementUnits = []
+        async.whilst(
+            function test(cb) {
+                cb(null, count > 0 || (i++) == 0)
+            },
+            function iter(callbackIter) {
+                var query = {
+                    query: {match_all: {}},
+                    size: fetchSize,
+                    from: from
+                }
+                from += fetchSize;
 
-    annotateCorpus:function(options,callback){
-        var query={
-            query:{match_all:{[options.contentField]:{}}
+                var options = {
+                    method: 'POST',
+                    json: query,
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+
+                    url: globalOptions.elasticUrl + globalOptions.corpusIndex + "/_search"
+                };
+
+                request(options, function (error, response, body) {
+                    if (error)
+                        return callbackIter(error);
+                    var hits = body.hits.hits;
+                    var annotatedHits = [];
+                    async.series([
+
+                        //extract regex arrays
+                        function (callbackSeries) {
+                            annotator_regex.extractRegexEntities(hits, globalOptions.regexConfig, function (err, result) {
+                                if (err)
+                                    return callbackSeries(err);
+                                annotatedHits = annotatedHits.concat(result);
+                                return callbackSeries()
+                            })
+                        },
+
+                        function (callbackSeries) {
+                            var xx = annotatedHits;
+                            annotatedHits.forEach(function (hit) {
+                                var entities = hit.regexEntities.extracted;
+                                entities.forEach(function (entity) {
+                                    var str=entity.groups.prefix+"\t"+entity.groups.unit
+                                    if (allMeasurementUnits.indexOf(str)<0)
+                                        allMeasurementUnits.push(str)
+                                })
+
+                            })
+                            return callbackSeries();
+                        },
+                        function (callbackSeries) {
+                            return callbackSeries();
+                        },
+                        function (callbackSeries) {
+                            return callbackSeries();
+                        },
+                    ], function (err) {
+                        if (err)
+                            return callbackIter(err);
+                        count = hits.length;
+                        return callbackIter()
+
+                    })
+
+                })
 
 
-        }
+            },
+            function (err, n) {
+                if (err)
+                    return callback(err);
+                var xx=allMeasurementUnits;
+                return callback(null, "Done")
+            }
+        );
+
+    }
+    , extractRegexEntities: function (hits, regexConfig, callback) {
+
+        var regex = regexConfig.regex
+        var annotatedHits = [];
+        hits.forEach(function (hit, hitIndex) {
+            var array = [];
+            var regexEntities = [];
+            var str = hit._source[regexConfig.contentField];
+            while ((array = regex.exec(str)) != null) {
+                if (array.groups.value && (array.groups.prefix || array.groups.unit)) {
+
+                    regexEntities.push({index: array.index, groups: array.groups})
+                }
+
+            }
+            if (regexEntities.length > 0)
+                annotatedHits.push({id: hit._id, index: hit._index, regexEntities: {extracted: regexEntities}});
+        })
+        return callback(null, annotatedHits)
 
 
     }
 
 
-
-
-
-
 }
 
 
-module.exports=annotator_regex;
+module.exports = annotator_regex;
+
+var options = {
+
+    elasticUrl: "http://localhost:9200/",
+    corpusIndex: "gmec_par",
+    regexConfig: {
+        regex: /(?<value>([\+-]?((\d*\.\d+)|\d+))(E[\+-]?\d+)?)( (?<prefix>[PTGMkmunpf  °])?(?<unit>[a-zA-Z]+)?)?/gm,
+        contentField: "attachment.content"
+    }
+}
+
+/*var str="30 kg 378 psi 53.3 °C"
+var array = [];
+
+while ((array = options.regexConfig.regex.exec(str)) != null) {
+   var x=array;
+}
+*/
+
+annotator_regex.annotateCorpus(options, function (err, result) {
+    var x = allMeasurementUnits;
+})
+
+
+
+
+
+
+
+
 
--- public/js/statistics.js
+++ public/js/statistics.js
@@ -1,13 +1,314 @@
-var statistics=(function(){
-    var self={};
+var statistics = (function () {
+    var self = {};
+
+
+    self.clear = function () {
+        $("#infosDiv").html("");
+        $("#docsDiv").html("");
+        $("#infosDiv").html("");
+        //   $("#jstreeDiv").html("");
+        $("#textDiv").html("");
+    }
+
+
+    self.getEntitiesQuery = function (callback) {
+
+        var queryString = $("#queryStringInput").val();
+        var entityQueryString = $("#entityQueryStringInput").val();
+        if (queryString && queryString.length > 0) {
+
+            var queryDocs = {
+                query: {
+                    "query_string": {
+                        "query": queryString,
+                        "default_field": "attachment.content",
+                        "default_operator": "AND"
+                    }
+                },
+                size: 1000,
+                _source: "_id"
+
+            }
+            var payload = {
+                executeQuery: JSON.stringify(queryDocs),
+                indexes: JSON.stringify(["gmec_par"])
+
+            }
+            $.ajax({
+                type: "POST",
+                url: appConfig.elasticUrl,
+                data: payload,
+                dataType: "json",
+                success: function (data, textStatus, jqXHR) {
+                    var xx = data;
+                    var docIds = [];
+                    data.hits.hits.forEach(function (hit) {
+                        docIds.push(hit._id)
+                    })
+                    var query = {
+                        terms: {
+                            ["documents.id"]: docIds
+                        }
+                    }
+                    return callback(null, query);
+
+                },
+                error: function (err) {
+                    console.log(err);
+                    return callback(err)
+                }
+            })
+
+        }
+        if (entityQueryString && entityQueryString.length > 0) {
+
+            var queryEntities = {
+                query: {
+                    "query_string": {
+                        "query": entityQueryString,
+                        "fields": ["internal_id", "synonyms"],
+                        "default_operator": "AND"
+                    }
+                }
+            }
+            var payload = {
+                executeQuery: JSON.stringify(queryEntities),
+                indexes: JSON.stringify(["thesaurus_ctg"])
+
+            }
+            $.ajax({
+                type: "POST",
+                url: appConfig.elasticUrl,
+                data: payload,
+                dataType: "json",
+                success: function (data, textStatus, jqXHR) {
+                    var xx = data;
+                    var docIds = [];
+                    data.hits.hits.forEach(function (hit) {
+                        hit._source.documents.forEach(function (doc) {
+                            docIds.push(doc.id)
+                        })
+
+                    })
+                    var query = {
+                        terms: {
+                            ["documents.id"]: docIds
+                        }
+                    }
+                    return callback(null, query);
+
+                },
+                error: function (err) {
+                    console.log(err);
+                    return callback(err)
+                }
+            })
+
+
+        } else {
+            var query = {
+                "match_all": {}
+            };
+            drawCanvas.drawAll({onclickFn: statistics.onRectClick, query: query});
+
+        }
+
+
+    }
+
+    self.drawHeatMap = function () {
+        self.clear();
+        self.getEntitiesQuery(function (err, result) {
+            drawCanvas.drawAll({onclickFn: statistics.onRectClick, query: result});
+        })
+    }
+
+    self.drawGraph = function () {
+        self.getEntitiesQuery(function (err, result) {
+            var url = "./heatMap"
+            if (result && result.terms && result.terms["documents.id"].length > 0) {
+                var ids = result.terms["documents.id"];
+                var query = {terms: {"documents.id": ids}}
+                var queryStr = encodeURIComponent(JSON.stringify(query))
+                url += "?query=" + queryStr
+            }
+
+            $.ajax({
+                type: "GET",
+                url: url,
+                dataType: "json",
+                success: function (data, textStatus, jqXHR) {
+
+                    var xx = data;
+                    var nodes=[];
+                    var edges=[];
+                    data.labels.forEach(function (label,labelIndex) {
+                        var node = {
+                            id:"E_"+labelIndex,
+                            color: "red",
+                            label: "" + label
+                        }
+                        nodes.push(node);
+                        data.data[labelIndex].forEach(function(col,colIndex){
+                            if(col>20){
+                               var edge ={
+                                   from:"E_"+labelIndex,
+                                   to:"E_"+colIndex,
+
+
+                               }
+                                edges.push(edge)
+                            }
+
+                        })
+
+                    })
+visjsGraph.draw("graphDiv",{nodes:nodes,edges:edges},{})
 
 
 
+                }, error: function (err) {
+                    $("#dialogDiv").html(err);
+                }
+            })
 
+        })
+    }
+
+
+    self.onRectClick = function (p, data) {
+        var labels = drawCanvas.rawData.labels;
+        var entity1 = labels[data.coords.x];
+        var entity2 = labels[data.coords.y];
+        var count = drawCanvas.rawData.data[data.coords.x][data.coords.y]
+        var str = "<b>" + entity1 + " / <br>" + entity2 + "<br>" + count + " </b>"//<button onclick=showDocs('" + entity1 + "','" + entity2 + "')>X</button>"
+        $("#infosDiv").html(str)
+        self.showDocs(entity1, entity2)
+    }
+
+    self.showDocs = function (entity1, entity2) {
+        var query = {
+            query: {
+                bool: {
+                    must: [
+                        {term: {"entities_thesaurus_ctg": entity1}},
+                        {term: {"entities_thesaurus_ctg": entity2}},
+                    ]
+                }
+            }
+            , size: 1000
+        }
+
+        var payload = {
+            executeQuery: JSON.stringify(query),
+            indexes: JSON.stringify(["gmec_par"])
+
+        }
+        $.ajax({
+            type: "POST",
+            url: appConfig.elasticUrl,
+            data: payload,
+            dataType: "json",
+            success: function (data, textStatus, jqXHR) {
+                var xx = data;
+
+                function docDataToJstree(data) {
+
+                    var docs = {};
+                    data.hits.hits.forEach(function (hit) {
+                        var docAttrs = hit._source
+                        if (!docs[docAttrs["docTitle"]]) {
+                            docs[docAttrs["docTitle"]] = {chapters: {}, docId: docAttrs.docId}
+
+                            if (!docs[docAttrs["docTitle"]].chapters[docAttrs["chapter"]]) {
+                                docs[docAttrs["docTitle"]].chapters[docAttrs["chapter"]] = {paragraphs: [], chapterId: docAttrs.chapterId}
+                            }
+                            docs[docAttrs["docTitle"]].chapters[docAttrs["chapter"]].paragraphs.push({text: docAttrs.text, paragraphId: docAttrs.paragraphId})
+                        }
+
+
+                    })
+
+                    var jsTreeArray = [];
+                    for (var docKey in docs) {
+                        var doc = docs[docKey];
+                        jsTreeArray.push({
+                            text: docKey, id: doc.docId, parent: "#"
+                        })
+                        for (var chapterKey in doc.chapters) {
+                            var chapter = doc.chapters[chapterKey];
+                            jsTreeArray.push({
+                                text: chapterKey, id: chapter.chapterId, parent: doc.docId
+                            })
+
+                            chapter.paragraphs.forEach(function (paragraph) {
+                                jsTreeArray.push({
+                                    text: paragraph.paragraphId, id: paragraph.paragraphId, parent: chapter.chapterId, data: {text: paragraph.text}
+                                })
+                            })
+
+                        }
+                    }
+                    return jsTreeArray;
+                }
+
+                var jsTreeArray = docDataToJstree(data);
+                self.drawJsTree("jstreeDiv", jsTreeArray)
+
+            }
+            , error: function (err) {
+
+                console.log(err.responseText)
+
+            }
+
+        });
+    }
+    self.drawJsTree = function (treeDiv, jsTreeData) {
+
+        var plugins = [];
+        plugins.push("search");
+
+        plugins.push("sort");
+        /*   plugins.push("types");
+           plugins.push("contextmenu");*/
+
+        if ($('#' + treeDiv).jstree)
+            $('#' + treeDiv).jstree("destroy")
+
+        $('#' + treeDiv).jstree({
+            'core': {
+                'check_callback': true,
+                'data': jsTreeData,
+
+
+            }
+            , 'contextmenu': {
+                'items': null
+            },
+            'plugins': plugins,
+            "search": {
+                "case_sensitive": false,
+                "show_only_matches": true
+            }
+        }).on("select_node.jstree",
+            function (evt, obj) {
+                var x = obj;
+                var html = obj.node.text
+                if (obj.node.data && obj.node.data.text)
+                    html = obj.node.data.text
+
+                $("#textDiv").html(html)
+                //   Entities.runEntityQuery(obj.node);
+                //   $("#dataDiv").html(JSON.stringify(obj.node.data,null,2))
+            })
+            .on('loaded.jstree', function () {
+                $("#" + treeDiv).jstree('open_all');
+            });
+    }
 
 
     return self;
-
 
 
 })()
